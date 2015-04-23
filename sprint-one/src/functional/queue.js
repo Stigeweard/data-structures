79723d04a7d4a1c7046973b639c7ec078af69d5d
C S Harding <charliesharding@gmail.com>
2015-04-23 08:56:53 -0500

    Queue is passing tests, stack is passing a few

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,22 @@ var Queue = function(){
 
   // Implement the methods below
 
+  var firstInLine = 0;
+  var lastInLine = 0;
+
   someInstance.enqueue = function(value){
+    storage[lastInLine] = value;
+    lastInLine++;
   };
 
   someInstance.dequeue = function(){
+    firstInLine++;
+    return storage[firstInLine-1];
   };
 
   someInstance.size = function(){
+    var lineLength = lastInLine - firstInLine;
+    return Math.max(0, lineLength);
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var stackSize = 0;
+  var topOfStack = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[topOfStack] = value;
+    topOfStack++;
   };
 
   someInstance.pop = function(){
+    topOfStack--;
+    return storage[topOfStack+1];
   };
 
   someInstance.size = function(){
+    return Math.max(topOfStack, 0);
   };
 
   return someInstance;
