af70457cad173f430b60ff9c2822b6ad27049c98
MKS-Student <mks.student@hackreactor.com>
2015-04-24 10:21:15 -0700

    Got linkedList to pass tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,40 @@
 var LinkedList = function(){
   var list = {};
+  // var posArray = []; // added
+  // var posInList = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.head === null){
+      list.head = new Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = new Node(value);
+      list.tail = list.tail.next;
+    }
+    list.tail.value = value;
   };
 
   list.removeHead = function(){
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
+    
   };
 
   list.contains = function(target){
+
+    var current = list.head;
+
+    while(current !== null) {
+      if(current.value === target){
+        return true;
+      } else { 
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
