5eb9461e4aaddf23ed0db5736e786a15cdb378c3
MKS-Student <mks.student@hackreactor.com>
2015-04-25 15:43:48 -0700

    Finished up to .set of hash tables (did not get to binary search tree)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,41 +1,57 @@
 
-
 var Graph = function(){
-	this.nodes = {};
 };
 
+//create a container for the nodes
+Graph.prototype.newGraph = {};
+
 Graph.prototype.addNode = function(node){
-	this.nodes[nodeKey] = node;
+	//create empty array for the node 
+	//add the node to 
+	this.newGraph[node] = [];
+
 };
 
 Graph.prototype.contains = function(node){
-	if(this.nodes.indexOf(node) >= 0){
+	//if node in graph return true 
+	if(node in this.newGraph) {
+		return true
+	}
+	return false;
+
+};
+
+Graph.prototype.removeNode = function(node){
+	//remove node from graph
+	//call removeEdge
+	delete this.newGraph[node];
+};
+
+Graph.prototype.hasEdge = function(fromNode, toNode){
+	//if fromNode has toNode and vice versa return true
+	//else return false
+	if(this.newGraph[fromNode].indexOf(toNode) >= 0){
 		return true;
 	} else {
 		return false;
 	}
 };
 
-Graph.prototype.removeNode = function(node){
-	if(this.nodes.indexOf(node) >= 0){
-		this.nodes.splice(this.nodes.indexOf(node), 1);
-	}
-};
-
-Graph.prototype.hasEdge = function(fromNode, toNode){
-	if (fromNode.edge !== null){
-		return true;
-	}
-};
-
 Graph.prototype.addEdge = function(fromNode, toNode){
-	fromNode.edge = toNode;
+	//create connection between fromNode and toNode
+	this.newGraph[fromNode].push(toNode)
+	this.newGraph[toNode].push(fromNode)
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	//remove fromNode from toNode and vice versa
 };
 
 Graph.prototype.forEachNode = function(cb){
+	//for each node in graph execute callback
+	for (key in this.newGraph){
+		cb(key);
+	}
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -4,17 +4,31 @@ var Set = function(){
   return set;
 };
 
-var setPrototype = {};
+var setPrototype = {
+};
 
 setPrototype.add = function(item){
+	if (this._storage === undefined) {
+		this._storage = [item];
+	} else if (this._storage.indexOf(item) < 0) {
+		this._storage.push(item);
+	}
 };
 
 setPrototype.contains = function(item){
+	if(this._storage.indexOf(item) >= 0){
+		return true;
+	} else {
+		return false;
+	}
 };
 
 setPrototype.remove = function(item){
+	var index = this._storage.indexOf(item)
+	if(index >= 0){
+		this._storage.splice(index, 1);
+	}
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
